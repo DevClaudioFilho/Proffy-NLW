64e6c7f611a5782678d42fb3c0af4b93170d84a6
DevClaudioFilho <devclaudiofilho@gmail.com>
2020-08-08 15:30:39 +0100

    day3

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,54 @@ import React from 'react';
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+    avatar:string,
+    bio:string,
+    cost:number,
+    id:number,
+    name:string,
+    subject:string,
+    whatsapp:string
+}
+interface TeacherItemProps{
+  teacher:Teacher
+}
+
+const TeacherItem: React.FC <TeacherItemProps>= ({teacher}) => {
+  const createNewConnection= async() =>{
+    await api.post('connections',{
+      user_id: teacher.id
+    })
+  }
+  
   return (
     <article className="teacher-item">
           <header>
             <img 
-              src="https://avatars3.githubusercontent.com/u/53623914?s=460&u=dd48ce77830daf259dabbdde3edad231603c5405&v=4" 
-              alt=""
+              src={teacher.avatar} 
+              alt={teacher.name}
             />
             <div>
-              <strong>Claudio</strong>
-              <span>Vida</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header>
           <p>
-            Espezializado em fazer cagadas
-            <br/><br/>
-            COm mais de 22anos de experiencia na arte de viveer e ensinando a ser foda
+           {teacher.bio}
           </p>
           <footer>
             Preço/hora
-            <strong>R$80,00</strong>
-            <button type="button">
+            <strong>`R$ {teacher.cost}`</strong>
+            <a 
+              target="blank"
+              onClick={createNewConnection} 
+              href={`https://wa.me/${teacher.whatsapp}`}
+            >
               <img src={whatsappIcon} alt="Whatsapp"/>
               Entrar em contato
-            </button>
+            </a>
           </footer>
         </article>
   );
